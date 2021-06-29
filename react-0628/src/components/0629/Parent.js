049e2b7d2ac027d63aabc7aa5f3ce62219840b3c
alex <alex3889660@gmail.com>
2021-06-29 23:11:14 +0800

    useState 的子父屬性Props 資料流向、callback函式

---
+++ react-0628/src/components/0629/Parent.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ChildA from './ChildA';
+import ChildB from './ChildB';
+
+function Parent() {
+  const [parentData, setParentData] = useState('Parent Data');
+
+  return (
+    <>
+      <ChildA parentData={parentData} />
+      <ChildB setParentData={setParentData} />
+    </>
+  );
+}
+
+export default Parent;
+
+// import React, { useState } from 'react';
+// import ChildA from './ChildA';
+// import ChildB from './ChildB';
+
+// function Parent() {
+//   const [parentData, setParentData] = useState('Parent Data');
+
+//   return (
+//     <>
+//       <ChildA parentData={parentData} />
+//       <ChildB setParentData={setParentData} />
+//     </>
+//   );
+// }
+
+// export default Parent;
